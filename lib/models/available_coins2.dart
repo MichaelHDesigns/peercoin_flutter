345b280e73109ad45f62adf2ba9134d33edced52
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-11-10 20:40:05 -0500

    Update available_coins2.dart

--- lib/models/available_coins2.dart
+++ lib/models/available_coins2.dart
@@ -5,28 +5,28 @@ import 'coin.dart';
 
 class AvailableCoins {
   static final Map<String, Coin> _availableCoinList = {
-  'sumcoin': Coin(
-    name: 'sumcoin',
-    displayName: 'Sumcoin',
-    uriCode: 'sumcoin',
-    letterCode: 'SUM',
+  'hthcoin': Coin(
+    name: 'hthcoin',
+    displayName: 'Hthcoin',
+    uriCode: 'hthcoin',
+    letterCode: 'HTH',
     iconPath: 'assets/icon/sum-icon-white-64.png',
     iconPathTransparent: 'assets/icon/sum-icon-white-64.png',
     networkType: NetworkType(
-      messagePrefix: '\Sumcoin Signed Message:\n',
+      messagePrefix: '\HomelessCoin Signed Message:\n',
       bech32: 'sum',
       bip32: Bip32Type(public: 0x0488b41c, private: 0x0488abe6),
-      pubKeyHash: 0x3F,
-      scriptHash: 0x05,
-      wif: 0xBF,
+      pubKeyHash: 0x64,
+      scriptHash: 0x28,
+      wif: 0xe4,
       opreturnSize: 100,
     ),
     fractions: 8,
     minimumTxValue: 10000,
     feePerKb: 0.001,
-    explorerUrl: 'https://insight.sumcore.org',
+    explorerUrl: 'http://154.12.237.243:3001/insight/',
     genesisHash:
-        '37d4696c5072cd012f3b7c651e5ce56a1383577e4edacc2d289ec9b25eebfd5e',
+        '37540c3c757bb77e42c168d8197447b6aba38c2d1ec0ddf59d2e774c41953093',
     txVersion: 1,
   ),
   'peercoin': Coin(
